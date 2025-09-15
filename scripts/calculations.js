346fb1f38274e48e061d4b1501c2776bb0931b99
daniel55247 <daniel55247@gmail.com>
2025-09-14 23:21:13 -0500

    Primera entrega

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,7 +1,7 @@
 let budgetValue = 0;
 let totalExpensesValue = 0;
 const expenseEntries = [["groceries", 33],["restaurants", 50],["transport",12],["home",70],["subscriptions", 14], ["groceries",28],["subscriptions", 12] ] ;
-let balanceColor = "";
+let balanceColor = "green";
 
 function sumGastos(){
     totalExpensesValue=0;
